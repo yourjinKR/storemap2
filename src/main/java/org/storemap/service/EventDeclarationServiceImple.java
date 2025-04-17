30f075e1c8eacd46ad4ff73a50e4b6b442c409a8
wigeon <juhyeon2398@naver.com>
2025-04-17 13:16:02 +0900

    어노테이션 추가

--- src/main/java/org/storemap/service/EventDeclarationServiceImple.java
+++ src/main/java/org/storemap/service/EventDeclarationServiceImple.java
@@ -1,5 +1,11 @@
 package org.storemap.service;
 
+import org.springframework.stereotype.Service;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Service
 public class EventDeclarationServiceImple implements EventDeclarationService{
 
 }
