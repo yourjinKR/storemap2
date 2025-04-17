c0871e4358aaedd0b47f19bab02889af9b981dae
yourjin <you720223721@gmail.com>
2025-04-17 11:48:39 +0900

    Controller add

---
+++ src/main/java/org/storemap/controller/AdminController.java
@@ -0,0 +1,13 @@
+package org.storemap.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@RequestMapping("admin/")
+@Controller
+public class AdminController {
+
+}
---
+++ src/main/java/org/storemap/controller/MemberController.java
@@ -0,0 +1,13 @@
+package org.storemap.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@RequestMapping("member/")
+@Controller
+public class MemberController {
+}
