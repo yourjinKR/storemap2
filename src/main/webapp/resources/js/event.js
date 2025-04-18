dc552bf142fb8a377aa85db504761294f26413ca
wigeon <juhyeon2398@naver.com>
2025-04-18 10:11:38 +0900

    event js

---
+++ src/main/webapp/resources/js/event.js
@@ -0,0 +1,7 @@
+const EVENT_CSS_FILE_PATH = '/resources/css/event.css';
+let linkEle = document.createElement('link');
+linkEle.rel = 'stylesheet';
+linkEle.href = EVENT_CSS_FILE_PATH;
+document.head.appendChild(linkEle);
+
+
