fce20828406c8988cdfa5bee5c390f7067bb339b
김정재 <buddy1997@naver.com>
2025-04-28 11:22:12 +0900

    점주 - 신청요청 mapper작성

--- src/main/java/org/storemap/mapper/StoreRequestMapper.java
+++ src/main/java/org/storemap/mapper/StoreRequestMapper.java
@@ -5,5 +5,12 @@ import java.util.List;
 import org.storemap.domain.StoreRequestVO;
 
 public interface StoreRequestMapper {
+	// 점주 신청 요청 등록
+	public int insert(StoreRequestVO vo);
+	// 점주 신청 요청 수락
+	public int update(int member_idx);
+	// 점주 신청 요청 삭제
+	public int delete(int member_idx);
+	// 점주 신청 요청 리스트 불러오기
 	public List<StoreRequestVO> getDisReqList();
 }
--- src/main/java/org/storemap/service/StoreRequestServiceImple.java
+++ src/main/java/org/storemap/service/StoreRequestServiceImple.java
@@ -15,6 +15,27 @@ public class StoreRequestServiceImple implements StoreRequestService{
 	@Autowired
 	StoreRequestMapper mapper;
 	
+	@Override
+	public int register(StoreRequestVO vo) {
+		log.info("register..."+vo);
+		int result = mapper.insert(vo);
+		return result;
+	}
+	
+	@Override
+	public int modify(int member_idx) {
+		log.info("modify..."+member_idx);
+		int result = mapper.update(member_idx);
+		return result;
+	}
+	
+	@Override
+	public int remove(int member_idx) {
+		log.info("remove..."+member_idx);
+		int result = mapper.delete(member_idx);
+		return result;
+	}
+	
 	public List<StoreRequestVO> getDisReqList() {
 		log.info("getDisReqList...");
 		return mapper.getDisReqList();
