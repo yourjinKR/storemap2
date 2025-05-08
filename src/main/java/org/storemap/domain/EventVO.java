b0c32c8aa2cc530add70cb245a305abe812f63e2
wigeon <juhyeon2398@naver.com>
2025-05-08 10:06:34 +0900

    eventVO수정

--- src/main/java/org/storemap/domain/EventVO.java
+++ src/main/java/org/storemap/domain/EventVO.java
@@ -11,8 +11,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class EventVO {
-	private int event_idx, store_idx, enter_idx, event_location, event_like_cnt, event_hidden;
-	private String event_title, event_category, event_location_detail, event_content, event_file;
+	private int event_idx, store_idx, enter_idx, event_postcode, event_like_cnt, event_hidden;
+	private String event_title, event_category, event_location, event_location_detail, event_content, event_file;
 	private Date event_rstartdate, event_rstopdate, event_bstartdate, event_bstopdate;
 	
 	// 게시글 등록 시 필요한 필드
