0fd5fd16fb4a71b532f5ed42e3eb581c5af65ac5
wigeon <juhyeon2398@naver.com>
2025-05-22 15:55:48 +0900

    중복 메소드 제거

--- src/main/java/org/storemap/mapper/EventRequestMapper.java
+++ src/main/java/org/storemap/mapper/EventRequestMapper.java
@@ -12,12 +12,9 @@ public interface EventRequestMapper {
 	public int cancelEntry(Map<String, Object> paramMap);
 	
 	public List<Integer> selectAppliedEdayIdxList(@Param("store_idx") int storeIdx);
-	public int cancelEntry(@Param("eday_idx") int eday_idx, @Param("store_idx") int store_idx);
 	
 	// 입점 신청 리스트
 	public List<EventRequestVO> getEdayRequest(int eday_idx);
 	// 입점 승인
-	public int updateRequest(int eday_idx, int store_idx);
-	// 입점 반려
-	public int deleteRequest(int eday_idx, int store_idx);
+	public int updateRequest(Map<String, Integer> map);
 }
