1893b6a7748125f55f7ca9f89a23a06c0925cc03
김정재 <buddy1997@naver.com>
2025-05-26 11:04:09 +0900

    store_open컬럼추가

--- src/main/java/org/storemap/domain/StoreVO.java
+++ src/main/java/org/storemap/domain/StoreVO.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class StoreVO {
-	private int store_idx, member_idx, store_like_cnt, store_hidden;
+	private int store_idx, member_idx, store_like_cnt, store_hidden, store_open;
 	private String store_name, store_image, store_num, store_email, store_address, store_area, store_activity_time, store_content, store_rnum, store_regcode;
 	private double store_lat, store_lng;
 	
