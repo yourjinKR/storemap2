96802b5fa9f68b2ec659879cc6c3176ee4ae34e6
wigeon <juhyeon2398@naver.com>
2025-05-28 14:16:25 +0900

    요청 쪽지전송

--- src/main/java/org/storemap/service/EnterRequestServiceImple.java
+++ src/main/java/org/storemap/service/EnterRequestServiceImple.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.storemap.domain.EnterRequestVO;
+import org.storemap.domain.EnterVO;
+import org.storemap.domain.LetterVO;
+import org.storemap.domain.MemberVO;
 import org.storemap.mapper.EnterRequestMapper;
 
 import lombok.extern.log4j.Log4j;
@@ -19,6 +22,14 @@ public class EnterRequestServiceImple implements EnterRequestService{
 	public int register(int enter_idx) {
 		log.info("register..."+enter_idx);
 		int result = mapper.insert(enter_idx);
+		EnterVO evo = memberMapper.read(member_idx);
+		if(result == 1) {
+			LetterVO vo = new LetterVO();
+			vo.setLetter_writer(mvo.getMember_id());
+			vo.setLetter_content("입점 요청이 접수되었습니다.");
+			vo.setLetter_receiver("admin");
+			letterMapper.insertLetter(vo);
+		}
 		return result;
 	}
 	
--- src/main/java/org/storemap/service/StoreRequestServiceImple.java
+++ src/main/java/org/storemap/service/StoreRequestServiceImple.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.storemap.domain.LetterVO;
+import org.storemap.domain.MemberVO;
 import org.storemap.domain.StoreRequestVO;
+import org.storemap.mapper.LetterMapper;
+import org.storemap.mapper.MemberMapper;
 import org.storemap.mapper.StoreRequestMapper;
 
 import lombok.extern.log4j.Log4j;
@@ -13,12 +17,24 @@ import lombok.extern.log4j.Log4j;
 @Service
 public class StoreRequestServiceImple implements StoreRequestService{
 	@Autowired
-	StoreRequestMapper mapper;
+	private StoreRequestMapper mapper;
+	@Autowired
+	private LetterMapper letterMapper;
+	@Autowired
+	private MemberMapper memberMapper;
 	
 	@Override
 	public int register(int member_idx) {
 		log.info("register..."+member_idx);
 		int result = mapper.insert(member_idx);
+		MemberVO mvo = memberMapper.read(member_idx);
+		if(result == 1) {
+			LetterVO vo = new LetterVO();
+			vo.setLetter_writer(mvo.getMember_id());
+			vo.setLetter_content("입점 요청이 접수되었습니다.");
+			vo.setLetter_receiver("admin");
+			letterMapper.insertLetter(vo);
+		}
 		return result;
 	}
 	
@@ -33,6 +49,14 @@ public class StoreRequestServiceImple implements StoreRequestService{
 	public int remove(int member_idx) {
 		log.info("remove..."+member_idx);
 		int result = mapper.delete(member_idx);
+		MemberVO mvo = memberMapper.read(member_idx);
+		if(result == 1) {
+			LetterVO vo = new LetterVO();
+			vo.setLetter_writer(mvo.getMember_id());
+			vo.setLetter_content("입점 요청이 반려되었습니다.");
+			vo.setLetter_receiver("admin");
+			letterMapper.insertLetter(vo);
+		}
 		return result;
 	}
 	
